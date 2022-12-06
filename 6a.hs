ef293a63c6836d234d9ba5fb89b4297bb1ee6ad3
Roland Peelen <roland@peppr.nl>
2022-12-06 10:08:02 +0100

    :art: - 6

---
+++ 6a.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Set as S
+import Lib
+import Prelude
+
+main :: IO ()
+main = do
+  input <- getContents
+  (print . fn) input
+
+fn :: [Char] -> Int
+fn = go 0
+  where
+    messageWidth = 4
+    go idx [] = idx
+    go idx xs | S.size (S.fromList (take messageWidth xs)) == messageWidth = idx + messageWidth
+    go idx (_x1 : xs) = go (idx + 1) xs
